c83e01c18aadf2c74cb630d31a34578e65a7256e
Hori <halimonevgeniy@gmail.com>
2021-10-16 14:18:03 +0300

    added CurrencyCourse

--- src/components/CurrencyCourse.js
+++ src/components/CurrencyCourse.js
@@ -1,8 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {Table} from 'react-bootstrap';
 
 function CurrencyCourse(){
-    return(
-        <h1>Currency course</h1>
+    const date = new Date()
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+
+    const [error, setError] = useState(null)
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+    fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json&date=${year}${month}${day}`)
+        .then(res => res.json())
+        .then(
+        (result) => {
+            setIsLoaded(true)
+            setItems(result)
+        },
+        (error) => {
+            setIsLoaded(true)
+            setError(error)
+        }
+        )
+    }, [])
+
+    if (error) {
+        return <div>Ошибка: {error.message}</div>
+    } else if (!isLoaded) {
+    return <div className="d-flex justify-content-center">
+            <div className="spinner-border text-secondary" role="status">
+            </div>
+            </div>
+    } 
+    return (
+        <Table striped bordered hover size="sm">
+        <thead>
+        <tr>
+            <th>Currency name</th>
+            <th>Currency code</th>
+            <th>Exchange course</th>
+        </tr>
+        </thead>
+        <tbody>
+        {items.map(item => (
+            <tr key={item.r030}>
+                <td>{item.txt}</td>
+                <td>{item.cc}</td>
+                <td>{item.rate.toFixed(3)}</td>
+            </tr>
+        ))}
+        </tbody>
+    </Table>
     )
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Navbar, Container, Nav, NavItem} from 'react-bootstrap'
+import {Navbar, Container, Nav, NavItem} from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
 function Navigation(){
